5c91ee4772368b8b019d79bda82f9be74a729f70
woblob <kitfaszon@interia.pl>
2020-09-23 18:44:32 +0200

    made class out of all the functions and cleaned it up

--- funkcje.py
+++ funkcje.py
@@ -1,75 +1,122 @@
 import subprocess
 import re
 import datetime
+from time import sleep
+import json
+# from collections import defaultdict
 
 
-def make_session(time):
-    activities = {
-        "tag": "session",
-        "start": time,
-        "end": None,
-        "activities": dict()
-    }
-    return activities
+class Timer:
+    def __init__(self, sleeping_time):
+        self.today = datetime.datetime.today()
+        self.session = self._make_session()
+        self.previous_window_name = "Desktop"
+        self.active_window_name = Timer._get_active_window_name()
+        self.start_time = None
+        self.end_time = None
+        self.sleeping_time = sleeping_time
 
-def make_activity(NAME):
-    activity = {
-        "tag": "activity",
-        "name": NAME,
-        "entries": dict(),
-    }
-    return activity
+    # @property
+    # def active_window_name(self):
+    #     return Timer._get_active_window_name()
+    #
+    # @active_window_name.setter
+    # def active_window_name(self, val):
+    #     self.active_window_name = val
 
-def make_entry(START, END):
-    s = START.strftime("%Y-%m-%d %H:%M:%S")
-    e = END.strftime("%Y-%m-%d %H:%M:%S")
-    entry = {
-        "tag": "entry",
-        "start_time": s,
-        "end_time": e,
-    }
-    return entry
+    def _make_session(self):
+        activities = {
+            "tag": "session",
+            "start": self.today.strftime("%Y-%m-%d %H:%M:%S"),
+            "end": None,
+            "activities": dict()
+        }
+        return activities
 
-def get_active_window_title():
-#     WM_WINDOW_ROLE(STRING) = "browser"
-    with subprocess.Popen(['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout=subprocess.PIPE) as root:
-        stdout, stderr = root.communicate()
-        m = re.search(b'^_NET_ACTIVE_WINDOW.* ([\w]+)\, 0x\w+\n$', stdout)
+    @staticmethod
+    def _make_activity(name):
+        activity = {
+            "tag": "activity",
+            "name": name,
+            "entries": [],
+        }
+        return activity
+
+    def _make_entry(self):
+        self.end_time = Timer._my_timer()
+        entry = {
+            "tag": "entry",
+            "start_time": self.start_time.strftime("%Y-%m-%d %H:%M:%S"),
+            "end_time": self.end_time.strftime("%Y-%m-%d %H:%M:%S"),
+        }
+        self.start_time = Timer._my_timer()
+        return entry
+
+    def _update_activity(self):
+        activities = self.session["activities"]
+        pwn = self.previous_window_name
+        if pwn not in activities:
+            activities[pwn] = Timer._make_activity(pwn)
+
+        new_entry = self._make_entry()
+        activities[pwn]["entries"].append(new_entry)
+        self.previous_window_name = self.active_window_name
+
+    def main_loop(self):
+        self.start_time = Timer._my_timer()
+        try:
+            _monitor_is_on = self._monitor_is_on()
+            while next(_monitor_is_on):
+                self.active_window_name = Timer._get_active_window_name()
+                if self.previous_window_name != self.active_window_name:
+                    self._update_activity()
+
+        except KeyboardInterrupt:
+            self._update_activity()
+            self._save_output()
+
+    def _save_output(self):
+        filename = self.end_time
+        with open(f"outputs/{filename}.json", "w") as file:
+            json.dump(self.session, file, indent=4)
+
+    @staticmethod
+    def _my_timer():
+        return datetime.datetime.now()
+
+    @staticmethod
+    def _get_active_window_name():
+        window_id = Timer._get_active_window_id()
+        name = Timer._get_window_name(window_id)
+        return name
+
+    @staticmethod
+    def _get_active_window_id():
+        with subprocess.Popen(['xprop', '-root', '_NET_ACTIVE_WINDOW'],
+                              stdout=subprocess.PIPE) as root:
+            stdout, stderr = root.communicate()
+        m = re.search(b'^_NET_ACTIVE_WINDOW.* (\w+), 0x\w+\n$', stdout)
         if m is None:
             return None
         window_id = m.group(1)
-#     just check id
-#     if no id in dictionary then look for it 
-    with subprocess.Popen(['xprop', '-id', window_id, 'WM_NAME'], stdout=subprocess.PIPE) as window:
-        stdout, stderr = window.communicate()
+        return window_id
+
+    @staticmethod
+    def _get_window_name(id_num):
+        with subprocess.Popen(['xprop', '-id', id_num, 'WM_NAME'],
+                              stdout=subprocess.PIPE) as window:
+            stdout, stderr = window.communicate()
         match = re.match(b"WM_NAME\(\w+\) = (?P<name>.+)$", stdout)
         if match is not None:
             name = match.group("name").strip(b'"').decode("utf-8")
-    return name or None
-
-def get_active_window_id():
-#     WM_WINDOW_ROLE(STRING) = "browser"
-    with subprocess.Popen(['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout=subprocess.PIPE) as root:
-        stdout, stderr = root.communicate()
-    m = re.search(b'^_NET_ACTIVE_WINDOW.* ([\w]+)\, 0x\w+\n$', stdout)
-    if m is None:
+            return name or None
         return None
-    window_id = m.group(1)
-    return window_id
 
-def get_active_window_title2(ID):
-#     if no id in set then look for it 
-    with subprocess.Popen(['xprop', '-id', ID, 'WM_NAME'], stdout=subprocess.PIPE) as window:
-        stdout, stderr = window.communicate()
-    match = re.match(b"WM_NAME\(\w+\) = (?P<name>.+)$", stdout)
-    if match is not None:
-        name = match.group("name").strip(b'"').decode("utf-8")
-    return name or None
-
-def monitor_is_on():
-    while True:
-        with subprocess.Popen(['xset', 'q'], stdout=subprocess.PIPE) as root:
-            stdout, stderr = root.communicate()
+    def _monitor_is_on(self):
+        while True:
+            with subprocess.Popen(['xset', 'q'],
+                                  stdout=subprocess.PIPE) as xset_dpms:
+                stdout, stderr = xset_dpms.communicate()
             match = re.search(b'Monitor is (?P<status>.+)$', stdout)
             if match is not None:
                 status = match.group("status")
@@ -77,40 +124,8 @@ def monitor_is_on():
             else:
                 yield False
 
-def prepare_session(data):
-    today = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-    session = make_session(today)
-    data[today] = session
+            sleep(self.sleeping_time)
 
-    return session
-
-def mytimer():
-    return datetime.datetime.now()
-
-def update_activity(activs, name, start, end):
-    if name not in activs:
-        activs[name] = make_activity(name)
-
-    entries = activs[name]["entries"]
-    new_entry_num = len(entries)
-    entries[new_entry_num] = make_entry(start, end)
-    
-def main_loop(activities):
-    previous_window_name = 'Desktop'
-    start_time = mytimer()
-    for _ in range(10): # 60 * 10
-        active_window_name = get_active_window_title()
-        if previous_window_name != active_window_name:
-            end_time = mytimer()
-            update_activity(activities, previous_window_name, start_time, end_time)
-            start_time = mytimer()
-            
-            previous_window_name = active_window_name
-        sleep(1)
-    
-    end_time = mytimer()
-    update_activity(activities, previous_window_name, start_time, end_time)
-    with open("outputs/filename.json", "w") as file:
-        json.dump(DATA, file, indent=4)
-        
-        
+if __name__ == "__main__":
+    T = Timer(3)
+    T.main_loop()
